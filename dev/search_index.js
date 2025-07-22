36084c72ba58b83fe4716deba88c0e338d3c730d
Documenter.jl <documenter@juliadocs.github.io>
2025-07-22 19:25:35 +0000

    build based on d0ceb23

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#Introduction","page":"About DocTest.jl","title":"Introduction","text":"","category":"section"},{"location":"","page":"About DocTest.jl","title":"About DocTest.jl","text":"Hello! This is a MWE package for testing documentation build and deployment with Documenter.jl and GitHub Actions","category":"page"},{"location":"","page":"About DocTest.jl","title":"About DocTest.jl","text":"","category":"page"}]
+[{"location":"#Introduction","page":"About DocTest.jl","title":"Introduction","text":"","category":"section"},{"location":"","page":"About DocTest.jl","title":"About DocTest.jl","text":"Hello! This is a MWE package for testing documentation build and deployment with Documenter.jl and GitHub Actions. 1","category":"page"},{"location":"","page":"About DocTest.jl","title":"About DocTest.jl","text":"","category":"page"}]
 }
